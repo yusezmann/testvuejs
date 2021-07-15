573a8ad949e7a24166cf39cde480e0d8baa7d6c2
Usman <37784103+yusezmann@users.noreply.github.com>
2021-07-15 18:24:06 +0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,7 +45,7 @@ export default {
           accent: colors.grey.darken3,
           secondary: colors.amber.darken3,
           info: colors.teal.lighten1,
-          warning: colors.amber.base,
+          warning: colors.yellow.base,
           error: colors.deepOrange.accent4,
           success: colors.green.accent3
         }
